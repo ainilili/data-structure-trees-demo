ab05ad2b3520a585c929fff171f11d7603c2d723
Nico <ainililia@163.com>
2019-04-10 17:52:28 +0800

    伸展树测试

--- src/test/java/org/smallnico/datastructure/tree/test/SplayTreeTest.java
+++ src/test/java/org/smallnico/datastructure/tree/test/SplayTreeTest.java
@@ -9,13 +9,14 @@ public class SplayTreeTest {
     @Test
     public void test() {
         AbstractBinaryTree bt = new SplayTree();
-        Integer[] array = new Integer[] {7,6,5,4,3,2,1};
+        Integer[] array = new Integer[] {1,32,30,31,28,29,26,27,24,25,22,23,20,21,18,19,16,17,14,15,12,13,10,11,8,9,6,7,4,5,2,3};
         for(int a: array) {
             bt.insert(a, a);
-            System.out.println("\n" + bt);
+//            System.out.println("\n" + bt);
         }
         
-        bt.get(1);
+        bt.get(2);
+        bt.get(3);
         System.out.println("\n" + bt);
         
 	}
